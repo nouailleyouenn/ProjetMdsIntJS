8ec21dcff91dd6a5eb56c1a5e19df24ac0f4de50
nouailleyouenn <117635999+nouailleyouenn@users.noreply.github.com>
2022-11-18 11:04:51 +0100

    home page html/js

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,83 @@
 
-const Home = { template: '<div>Home</div>' }
-const Articles={template:'<div>Articles</div>'}
-const creation={template:'<div>creation</div>'}
+const Home = {
+    data() {
+        return {
+            articles: [
+                { name: "na", catégorie: "na", texte: "na", auteur: "na", url: "na", image: "na" },
+                { name: "na2", catégorie: "na2", texte: "na2", auteur: "na2", url: "na2", image: "na2" }
+            ]
+        }
+
+    },
+    template: `
+
+<div>
+    Home
+</div>
+<div id="listeArticlesHome">
+    <ul>
+    <li v-for="article in articles">
+        {{article.name}}
+    </li>
+    </ul>
+</div>
+<div id="nomPrenomGroupe">
+    Youenn Nouaille / Adonis Manach
+</div>
+<div id="descEncyclo">
+    bref descriptions
+</div>
+
+`}
+
+const Articles = {
+    template: `
+
+<div>
+    Articles
+</div>
+
+`}
+
+const creation = {
+    template: `
+
+<div>
+    creation
+</div>
+
+`}
 
 
 
 
 
+const ArticlesListe = {
+    data() {
+        return {
+            articles: [
+                { name: "na", catégorie: "na", texte: "na", auteur: "na", url: "na", image: "na" },
+                { name: "na2", catégorie: "na2", texte: "na2", auteur: "na2", url: "na2", image: "na2" }
+            ]
+        }
 
+    },
+    template: `
+    <div>
+    <ul>
+    <li v-for="article in articles">
+        {{article.name}}
+    </li>
+    </ul>
+    </div>
+    
+    
+    
+    
+    
+    
+    `
+}
 
 
 const routes = [
@@ -21,9 +91,10 @@ const routes = [
             }
         ],
     },*/
-    {path:'/', component:Home}, //chemin de la page d'acceuil
-    {path:'/articles',component:Articles}, //chemin de la page artciles
-    {path:'/create',component:creation} //chemin de la page de création d'articles
+    { path: '/', component: Home }, //chemin de la page d'acceuil
+    { path: '/articles', component: Articles }, //chemin de la page artciles
+    { path: '/create', component: creation }, //chemin de la page de création d'articles
+    {path: '/listeArticles',component:ArticlesListe}  //chemin de la page de l'affichage d'articles
 ]
 
 

846be45e092b85631877e5f96e5b663a5e752942
nouailleyouenn <117635999+nouailleyouenn@users.noreply.github.com>
2022-11-24 09:13:56 +0100

    test ethan

--- JS/articlePage.js
+++ JS/articlePage.js
@@ -6,8 +6,14 @@ const articlePage= {
     <div> {{$route.params.categorie}}</div>
     <div> {{$route.params.texte}}</div>
     <div> {{$route.params.auteur}}</div>
+    <div> Source : <a>{{$route.params.url}}</a></div>
+
+    <img v-bind:src="image">
 
     
     `,
+    updated(){
+        image = $route.params.image
+    }
     
 }

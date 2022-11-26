f91f0c08308b43348f4935855b3e3188b7c35d48
nouailleyouenn <117635999+nouailleyouenn@users.noreply.github.com>
2022-11-26 19:04:49 +0100

    Less v1

--- JS/articlePage.js
+++ JS/articlePage.js
@@ -3,18 +3,18 @@ console.log("lancement")
 const articlePage= {
     //templage de la section article
     template :`
-    <div id="TitreSection-Article">{{$route.params.name}}</div>
-    <div id="CategorieArticle"> Catégorie : {{$route.params.categorie}}</div>
+    <div class="TitreSection-Article">{{$route.params.name}}</div>
+    <div class="CategorieArticle"> Catégorie : {{$route.params.categorie}}</div>
     
-    <div id="Text-Article" > {{$route.params.texte}}</div>
+    <div class="Text-Article" > {{$route.params.texte}}</div>
     <br>
     <div style="display: inline">  Auteur : {{$route.params.auteur}}</div>
     <a v-bind:href="urls" style="display: inline"> Source </a>
     
     <br>
     <br>
-    <div id="image-article">
-    <img v-bind:src="images" id="img3">
+    <div class="image-article">
+    <img v-bind:src="images" class="img3">
     </div>
     <br>   <br>   <br>   <br>   <br>   <br>   <br>   <br>
 
--- JS/home.js
+++ JS/home.js
@@ -2,10 +2,10 @@ const Home = {
     //template de la page d'acceuill
     template: `
 
-<div id="TitreSection">
+<div class="TitreSection">
     <div>Home</div>
 </div>
-<div id="listeArticlesHome" v-on="ListeArticles">
+<div class="listeArticlesHome" v-on="ListeArticles">
     <ul>
     <li v-for="article in articles" >
         <router-link :to="{name:'articleDesc',params:{id:articles.indexOf(article),name:article.name,categorie:article.categorie,texte:article.texte,auteur:article.auteur,url:article.url,image:article.image}}">{{article.name}}</router-link>
@@ -15,22 +15,22 @@ const Home = {
 <div>
 <router-view></router-view>
 </div>
-<div id="TableauHome">
+<div class="TableauHome">
 <table>
     <tr>
-        <td id="ImgHome"><img src="https://www.letelegramme.fr/images/2021/02/08/vannes-beneficie-des-aides-de-l-etat-dans-le-cadre-du-plan_5524054_676x337p.jpg?v=1" id="img2"/></td>
-        <td id="ImgHome"><img src="https://img.20mn.fr/l9O92uPMScSu2sAdrR7m8Sk/1200x768_ville-vannes-annule-defile-fetes-historiques-raison-canicule" id="img2"/></td>
+        <td class="ImgHome"><img src="https://www.letelegramme.fr/images/2021/02/08/vannes-beneficie-des-aides-de-l-etat-dans-le-cadre-du-plan_5524054_676x337p.jpg?v=1" class="img2"/></td>
+        <td class="ImgHome"><img src="https://img.20mn.fr/l9O92uPMScSu2sAdrR7m8Sk/1200x768_ville-vannes-annule-defile-fetes-historiques-raison-canicule" class="img2"/></td>
     </tr>
     <tr>
-        <td id="ImgHome"><img src="https://img.20mn.fr/l9O92uPMScSu2sAdrR7m8Sk/1200x768_ville-vannes-annule-defile-fetes-historiques-raison-canicule" id="img2"/></td>
-        <td id="ImgHome"><img src="https://www.bretagne.com/sites/default/files/post/Vannes_S.jpg" id="img2" /></td>
+        <td class="ImgHome"><img src="https://img.20mn.fr/l9O92uPMScSu2sAdrR7m8Sk/1200x768_ville-vannes-annule-defile-fetes-historiques-raison-canicule" class="img2"/></td>
+        <td class="ImgHome"><img src="https://www.bretagne.com/sites/default/files/post/Vannes_S.jpg" class="img2" /></td>
     </tr>
 
 </table>
 </div>
 <br><br><br><br><br>
-<div id="nomPrenomGroupeContainer">
-    <div id="nomPrenomGroupe"> Youenn Nouaille / Adonis Manach </div>
+<div class="nomPrenomGroupeContainer">
+    <div class="nomPrenomGroupe"> Youenn Nouaille / Adonis Manach </div>
 </div>
 
 
